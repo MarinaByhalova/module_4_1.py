7ddcad6def400bf24db5b07452cf1aec48d6540f
MarinaByhalova <mzhirnovsk@gmail.com>
2024-08-30 15:05:25 +0300

    Add files via upload

---
+++ true_math.py
@@ -0,0 +1,11 @@
+def divide(first, second):
+    try:
+        return first / second
+    except ZeroDivisionError:
+        if first == 0:
+            raise ValueError('0/0 is undefined')
+        if first > 0:
+            return float('inf')
+        else:
+            return float('-inf')
+        print(res)
